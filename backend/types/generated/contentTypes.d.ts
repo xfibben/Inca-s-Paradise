21613203d8e0bbed92fdb8f22c0f4f460a8beade
xfibben <eyck.arturo@icloud.com>
2026-04-01 15:36:23 -0500

    fixing transportes controler

--- backend/types/generated/contentTypes.d.ts
+++ backend/types/generated/contentTypes.d.ts
@@ -1480,30 +1480,6 @@ export interface ApiTransporteTransporte extends Struct.CollectionTypeSchema {
     };
   };
   attributes: {
-    adultUnitPrice: Schema.Attribute.Decimal &
-      Schema.Attribute.SetPluginOptions<{
-        i18n: {
-          localized: false;
-        };
-      }> &
-      Schema.Attribute.SetMinMax<
-        {
-          min: 0;
-        },
-        number
-      >;
-    childUnitPrice: Schema.Attribute.Decimal &
-      Schema.Attribute.SetPluginOptions<{
-        i18n: {
-          localized: false;
-        };
-      }> &
-      Schema.Attribute.SetMinMax<
-        {
-          min: 0;
-        },
-        number
-      >;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
@@ -1543,19 +1519,6 @@ export interface ApiTransporteTransporte extends Struct.CollectionTypeSchema {
           localized: false;
         };
       }>;
-    discount: Schema.Attribute.Decimal &
-      Schema.Attribute.SetPluginOptions<{
-        i18n: {
-          localized: false;
-        };
-      }> &
-      Schema.Attribute.SetMinMax<
-        {
-          min: 0;
-        },
-        number
-      > &
-      Schema.Attribute.DefaultTo<0>;
     distancia: Schema.Attribute.String &
       Schema.Attribute.SetPluginOptions<{
         i18n: {
