de61a1123edaca23eb7f34c94f8cac5707f56599
Arturo Eyck Tapia Ramos <eyck.arturo@icloud.com>
2026-05-04 12:19:28 -0500

    fix bug strapi subcategoria

--- backend/src/api/subcategoria-tour/controllers/subcategoria-tour.ts
+++ backend/src/api/subcategoria-tour/controllers/subcategoria-tour.ts
@@ -4,4 +4,4 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::subcategoria-tour.subcategoria-tour');
+export default (factories as any).createCoreController('api::subcategoria-tour.subcategoria-tour');
